976857ee4b1b4ded9861e068905fc3e46f8b0acb
lamnguyen5464 <lamnguyen5464@gmail.com>
2022-01-18 16:43:00 +0700

    init

--- StaticWebView-Demo/ViewController.swift
+++ StaticWebView-Demo/ViewController.swift
@@ -6,12 +6,23 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var webView: WKWebView!
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 
 

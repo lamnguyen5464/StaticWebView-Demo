00aa2808f560e84322fe45db45612bc6973582be
lamnguyen5464 <lamnguyen5464@gmail.com>
2022-01-18 16:36:43 +0700

    Initial Commit

---
+++ StaticWebView-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StaticWebView-Demo
+//
+//  Created by lam.nguyen5 on 1/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
